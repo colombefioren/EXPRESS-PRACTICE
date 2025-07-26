78545afcce448aed0cc6c68a0a24fe00dcb6e920
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-26 16:57:30 +0300

    feat: cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import charactersRouter from "./routes/characters.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/characters", charactersRouter);
 
 export default app;

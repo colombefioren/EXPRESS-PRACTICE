bd61537a5abc4cc1aea0ec3eec268b45e5112877
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-26 10:28:34 +0300

    fix: user.json is updating according to created character

--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-// GET /characters ==> Get all characters
-// POST /characters ==> Create a new character
-// GET /characters/:id ==> Get a character by ID
 // PUT /characters/:id ==> Update a character by ID
 // DELETE /characters/:id ==> Delete a character by ID
 
@@ -11,32 +8,54 @@ const PORT = 4000;
 
 app.use(express.json());
 
+// GET /characters ==> Get all characters
+
 app.get("/characters", async (req, res) => {
   fs.readFile("user.json", "utf-8", (err, data) => {
     if (err) {
       res.status(500).json({ message: "Error reading file!" });
       return;
-    } else {
-      try {
-        console.log(data);
-        const characterData = JSON.parse(data);
-        res.json(characterData);
-      } catch (error) {
-        console.error(error);
-      }
+    }
+    try {
+      const characterData = JSON.parse(data);
+      res.json(characterData);
+    } catch (ParsingError) {
+      console.error(error);
     }
   });
 });
 
+// POST /characters ==> Create a new character
+
 app.post("/characters", async (req, res) => {
-  try {
-    const data = await req.body;
-    const newCharacter = JSON.parse(data);
-    console.log(newCharacter);
-    res.json(newCharacter);
-  } catch (error) {
-    console.error(error);
-  }
+  fs.readFile("user.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Error reading file !" });
+      return;
+    }
+    try {
+      const characterData = JSON.parse(data);
+      const newCharacter = req.body;
+      characterData.characters.push(newCharacter);
+      fs.writeFile(
+        "user.json",
+        JSON.stringify(characterData, null, 2),
+        "utf-8",
+        (err) => {
+          if (err) {
+            res.status(500).json({ message: "Error writing file!" });
+          } else {
+            res.status(201).json(newCharacter);
+          }
+        }
+      );
+    } catch (ParsingError) {
+      console.error(ParsingError);
+    }
+  });
 });
+
+// GET /characters/:id ==> Get a character by ID
+
 
 app.listen(PORT, () => console.log(`Server running on the PORT ${PORT}`));
